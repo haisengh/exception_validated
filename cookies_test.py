6db421bd34d0fa99179911fd64fb9bb34ec51236
chao <haisengh@gmail.com>
2018-10-30 22:50:56 +0800

    passed the test, completing the fundamental function.

---
+++ cookies_test.py
@@ -0,0 +1,8 @@
+import requests
+url = "https://httpbin.org/cookies"
+
+# r = requests.get(url,cookies = cookies)
+# print(r.text)
+print("\u2026".encode("utf-8"))
+
+print(b'\xe2\x80\xa6'.decode("utf-8"))
